6389dcabbee00a9a4221d0332dd5687bbb71c11c
Murilo Bergamin <murilobergamin@hotmail.com>
2022-07-19 19:19:28 -0300

    validations/list controller and list views

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,16 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmarks = Bookmark.all
+  end
+
+  def create
+
+  end
+
+  def delete
+    @bookmark.destroy
+    redirect_to bookmarks_path
+  end
+
+
 end
